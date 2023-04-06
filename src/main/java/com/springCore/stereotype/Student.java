5d95793654dd6248f59bc13f9a8994ab976d55d4
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-04-06 20:11:48 +0530

    update

---
+++ src/main/java/com/springCore/stereotype/Student.java
@@ -0,0 +1,32 @@
+package com.springCore.stereotype;
+
+public class Student {
+	private String name;
+	private String city;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", city=" + city + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
