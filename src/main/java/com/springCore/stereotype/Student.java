fbb98e12a80874466a6e726113dcaffae9685316
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-04-08 19:39:59 +0530

    update stereotype annotations

--- src/main/java/com/springCore/stereotype/Student.java
+++ src/main/java/com/springCore/stereotype/Student.java
@@ -1,27 +1,51 @@
 package com.springCore.stereotype;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Student {
+	
+	@Value("Habibi")
 	private String name;
+	
+	@Value("goa")
 	private String city;
+	
+	@Value("#{add}")
+	private List<String> address;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
-	
-	
+
+	public List<String> getAddress() {
+		return address;
+	}
+
+	public void setAddress(List<String> address) {
+		this.address = address;
+	}
+
 	@Override
 	public String toString() {
-		return "Student [name=" + name + ", city=" + city + "]";
+		return "Student [name=" + name + ", city=" + city + ", address=" + address + "]";
 	}
-	
+
 	
 	
 	
--- src/main/java/com/springCore/stereotype/Test.java
+++ src/main/java/com/springCore/stereotype/Test.java
@@ -1,9 +1,19 @@
 package com.springCore.stereotype;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springCore/stereotype/stconfig.xml");
+		
+		Student student =  context.getBean("student",Student.class);
+		
+		System.out.println(student);
+		
 
 	}
 
