5698b8715a07f0c4583f992b17fcf5f1313b42d7
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-04-10 18:58:04 +0530

    SPEL

--- src/main/java/com/springCore/spel/SpelDemo.java
+++ src/main/java/com/springCore/spel/SpelDemo.java
@@ -12,6 +12,15 @@ public class SpelDemo {
 	@Value("#{15+10+15}")
 	private int y;
 	
+	@Value("#{ T(java.lang.Math).sqrt(25) }")	// call static methods
+	//@Value("#{ T(java.lang.Math).E }") //Call static variable
+	private double z;
+	
+	@Value("#{ new java.lang.String('Himanshu sharma')}")	//Create new object
+	private String name;
+	
+	@Value("#{8>5}")
+	private Boolean isMen;
 	
 	public int getX() {
 		return x;
@@ -25,15 +34,23 @@ public class SpelDemo {
 	public void setY(int y) {
 		this.y = y;
 	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
 	
 	
 	@Override
 	public String toString() {
-		return "SpelDemo [x=" + x + ", y=" + y + "]";
+		return "SpelDemo [x=" + x + ", y=" + y + ", z=" + z + ", name=" + name + ", men=" + isMen + "]";
 	}
+
+
 	
-	
-	
+
+
 	
 
 }
